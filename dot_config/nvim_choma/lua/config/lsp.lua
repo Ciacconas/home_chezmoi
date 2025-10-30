146cae9f91360f75946b24697536e72a38b196e4
Ciacconas <ma@chonghuai.me>
2025-10-29 22:19:38 -0400

    update

--- dot_config/nvim_choma/lua/config/lsp.lua
+++ dot_config/nvim_choma/lua/config/lsp.lua
@@ -3,8 +3,6 @@
 -- vim.lsp.enable('ruff')
 -- vim.lsp.enable('lua_ls')
 
-
-
 vim.diagnostic.config({
     virtual_text = true,
     underline = { severity = vim.diagnostic.severity.ERROR },
@@ -45,24 +43,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        map(
-            'gri',
-            require('telescope.builtin').lsp_implementations,
-            '[G]oto [I]mplementation'
-        )
+        map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
         -- Jump to the definition of the word under your cursor.
         --  To jump back, press <C-t>.
         map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
-
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
-        map(
-            'gO',
-            require('telescope.builtin').lsp_document_symbols,
-            'Open Document Symbols'
-        )
+        map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
@@ -75,13 +64,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        map(
-            'grt',
-            require('telescope.builtin').lsp_type_definitions,
-            '[G]oto [T]ype Definition'
-        )
-
-
+        map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
         -- CursorHold highlights ============================================
         local function client_supports_method(client, method, bufnr)
